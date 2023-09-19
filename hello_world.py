a2701ca9f775d7c54cee756511c41b51fa385677
Victoire de Lacharriere <v.ladreitdelacharriere@gmail.com>
2023-09-19 16:25:25 +0200

    google_analytics deployment

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! :)</p>"
+    var = """
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FVQS2TWEVX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FVQS2TWEVX');
+</script>
+    """
+    return "<p>Hello, World! :)</p>" + var
